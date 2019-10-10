b2b4ced21d2ef59bb3a43e9f1ae6ac68368cf1e3
Wes Fabella <wfabella@amazon.com>
2019-10-10 14:15:09 -0400

    init myiot thing

---
+++ cdk/boto3_helper.py
@@ -0,0 +1,115 @@
+
+import sys
+import cfnresponse
+import boto3, botostubs
+from botocore.exceptions import ClientError
+import json
+import logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+client: botostubs.IoT = boto3.client('iot') # type: botostubs.IOT
+
+# iot policy doc with greengrass support
+policyDocument = {
+    'Version': '2012-10-17',
+    'Statement': [
+        {
+            'Effect': 'Allow',
+            'Action': 'iot:*',
+            'Resource': '*'
+        },
+        {
+            'Effect': 'Allow',
+            'Action': 'greengrass:*',
+            'Resource': '*'
+        }
+    ]
+}
+
+# iot thing greengrass core
+core_policyDocument = {
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "iot:Publish",
+        "iot:Subscribe",
+        "iot:Connect",
+        "iot:Receive"
+      ],
+      "Resource": [
+        "*"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "iot:GetThingShadow",
+        "iot:UpdateThingShadow",
+        "iot:DeleteThingShadow"
+      ],
+      "Resource": [
+        "*"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "greengrass:*"
+      ],
+      "Resource": [
+        "*"
+      ]
+    }
+  ]
+}
+
+
+def myiot_create_thing(thingName):
+    responseData = {}
+
+    # boto3 create thing, create keys and device cert
+    client.create_thing(
+        thingName=thingName
+    )
+    response = client.create_keys_and_certificate(
+        setAsActive=True
+    )
+    certId = response['certificateId']
+    certArn = response['certificateArn']
+    certPem = response['certificatePem']
+    privateKey = response['keyPair']['PrivateKey']
+    client.create_policy(
+        policyName='{}-full-access'.format(thingName),
+        policyDocument=json.dumps(policyDocument)
+    )
+    response = client.attach_policy(
+        policyName='{}-full-access'.format(thingName),
+        target=certArn
+    )
+    response = client.attach_thing_principal(
+        thingName=thingName,
+        principal=certArn,
+    )
+    logger.info('Created thing: %s, cert: %s and policy: %s' % 
+        (thingName, certId, '{}-full-access'.format(thingName)))
+    responseData['certificateArn'] = certArn
+    responseData['certificateId'] = certId
+    responseData['certificatePem'] = certPem
+    responseData['privateKey'] = privateKey
+    responseData['iotEndpoint'] = client.describe_endpoint(endpointType='iot:Data-ATS')['endpointAddress']
+    return responseData
+
+
+def myiot_create_csr(): # ugh! - would like to avoid this but will require a custom resource
+    response = client.create_keys_and_certificate(
+        setAsActive=True
+    )
+    # certId = response['certificateId']
+    # certArn = response['certificateArn']
+    # certPem = response['certificatePem']
+    # privateKey = response['keyPair']['PrivateKey']
+    return response
---
+++ cdk/cdk_stack.py
@@ -0,0 +1,54 @@
+from aws_cdk import (
+    aws_iot as iot,
+    aws_s3 as s3,
+    core
+)
+import boto3, botostubs
+import boto3_helper
+
+class CdkStack(core.Stack):
+
+    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        # The code that defines your stack goes here
+
+        # create iot thing & policy
+        thing = iot.CfnThing(self,
+            "MyIoTCDKThing",
+            thing_name='myiot-thing'
+        )
+        print("## Thing Name: %s" % thing.thing_name)
+
+        thing_policy = iot.CfnPolicy(self,
+            "MyIoTCDKThingPolicy",
+            policy_document=boto3_helper.policyDocument,
+            policy_name='myiot-policy'
+        )
+
+        # debug - get policy name 
+        thing_policy_name = thing_policy.policy_name
+        print("## Policy Name: %s" % thing_policy_name)
+
+        # create iot thing certificate
+        response = boto3_helper.myiot_create_csr()  # ugh! - gens a new cert for each deployment
+        print(response)
+        thing_policy_principal_props = iot.CfnPolicyPrincipalAttachmentProps(
+            policy_name=thing_policy.policy_name,
+            principal=response['certificateArn']
+        )
+        print("## Principal Cert Arn: %s" % thing_policy_principal_props.principal)
+
+        # attach policy to iot certificate
+        iot.CfnPolicyPrincipalAttachment(self,
+            'MyIoTCDKPolicyPrincipalAttachment',
+            policy_name=thing_policy.policy_name,
+            principal=thing_policy_principal_props.principal
+        )
+
+        # attach certificate to iot thing
+        iot.CfnThingPrincipalAttachment(self,
+            'MyIoTCDKThingPrincipalAttachment',
+            thing_name=thing.thing_name,
+            principal=thing_policy_principal_props.principal
+        )
